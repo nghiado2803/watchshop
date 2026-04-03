57027dac26bbae29953fa265b8f7d6572214a35a
nghiado2803 <nghiadvpy00194@gmail.com>
2026-04-03 07:48:59 +0700

    Update Review.java

--- src/main/java/com/example/watchshop/entity/Review.java
+++ src/main/java/com/example/watchshop/entity/Review.java
@@ -20,17 +20,14 @@ public class Review {
     @Column(name = "created_at")
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    // 1. Liên kết với User (Ai đánh giá)
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    // 2. Liên kết với Product (Đánh giá sản phẩm nào)
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
 
-    // 3. Liên kết với Order (Thuộc đơn hàng nào - QUAN TRỌNG để chặn spam)
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
