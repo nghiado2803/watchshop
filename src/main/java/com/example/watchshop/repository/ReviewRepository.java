91484619877e8e98c1d13a483b9b9b121fd8ba62
nghiado2803 <nghiadvpy00194@gmail.com>
2026-04-03 07:49:26 +0700

    Update ReviewRepository.java

--- src/main/java/com/example/watchshop/repository/ReviewRepository.java
+++ src/main/java/com/example/watchshop/repository/ReviewRepository.java
@@ -7,10 +7,8 @@ import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    // Lấy tất cả đánh giá của 1 sản phẩm, sắp xếp mới nhất lên đầu
     List<Review> findByProductIdOrderByCreatedAtDesc(Long productId);
     List<Review> findByProductId(Long productId);
 
-    // Kiểm tra xem trong Đơn hàng X, sản phẩm Y đã được đánh giá chưa?
     boolean existsByOrderIdAndProductId(Long orderId, Long productId);
 }
